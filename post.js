edd0215e5fccfb409a22190df7164fa94b5536b7
Benjamin Edward Webb <benjamin.webb@hitachivantara.com>
2017-12-10 22:48:51 -0600

    add sample js files

---
+++ post.js
@@ -0,0 +1,31 @@
+function() {
+	    
+	    var me = this;
+
+	    /*
+		 *     *
+		 *         * Define events for mouse move
+		 *             *
+		 *                 */
+	    
+	    this.mapEngine.map.events.register('mousemove', this.mapEngine.map, function (e) {
+			        
+			        if (!_.isEmpty(me.currentFeatureOver)) {
+						           
+						            var modelItem = me.mapEngine.model.findWhere({id: me.currentFeatureOver.id});
+						   
+						            $('#popupObj').css('top', e.pageY -50).css('left', e.pageX + 5).html(modelItem.id + '(' + modelItem.attributes.data.value + ')');
+						           
+						        }
+			        
+			    });
+
+	    this.mapEngine.map.events.register('movestart', this.mapEngine.map, function (e) {
+			        $('#popupObj').fadeIn(500);
+			    });
+	    
+	    this.mapEngine.map.events.register('moveend', this.mapEngine.map, function (e) {
+			        $('#popupObj').fadeOut(500);
+			    });    
+	    
+} 
