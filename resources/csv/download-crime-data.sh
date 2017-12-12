9c88b50d5e8cf8d764a92bda8f3bec77a17bc9e5
Benjamin Edward Webb <benjamin.webb@hitachivantara.com>
2017-12-11 23:48:35 -0600

    aggregate chicago crime data for counts (edited to remove big files)
    remove big crime file
    
    add gitignores for datafiles

---
+++ resources/csv/download-crime-data.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+wget "https://data.cityofchicago.org/api/views/ijzp-q8t2/rows.csv?accessType=DOWNLOAD"
+mv "Crimes_-_2001_to_present.csv" "Crimes-2001-to-present.csv"
--- resources/js/loadUsCounties.js
+++
@@ -1,3 +0,0 @@
-define(['text!../geojson/us-counties-geojson.js'], function(content){    
-    return JSON.parse(content);
-})
